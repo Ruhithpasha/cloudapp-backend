c3d89696b6ad4fadf4f848b3619629d52a1306c2
ruhithpasha <ruhithpasha813@gmail.com>
2025-05-10 22:44:15 +0530

    commit

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 // CORS configuration
 app.use(cors({
-  origin: function(origin, callback) {
-    // Remove trailing slash if present
-    const cleanOrigin = origin?.replace(/\/$/, '');
-    const allowedOrigins = [
-      'https://cloudapp-frontend-kohl.vercel.app',
-      'http://localhost:5173'
-    ];
-    
-    if (!origin || allowedOrigins.includes(cleanOrigin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'https://cloudapp-frontend-kohl.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+// Add additional headers middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://cloudapp-frontend-kohl.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 // ... existing code ... 
